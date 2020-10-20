b7a0d6eed94945a0869f838006087ec5a8cf74e1
Miguel Alvarez <kamapu78@gmail.com>
2020-10-20 11:00:07 +0200

    Improved version of require_now()

--- R/require_now.R
+++ R/require_now.R
@@ -3,7 +3,8 @@
 #' @title Install packages only if not yet installed
 #' 
 #' @description 
-#' A list of packages will be compared with the installed
+#' A list of packages will be compared with the installed ones and if missing,
+#' installed. These packages will be also loaded in the current session.
 #' This function is a wrapper of [update.packages()] and [install.packages()].
 #' 
 #' @param pkgs Character vector, the names of the packages to be installed.
@@ -16,10 +17,23 @@
 #' @export require_now
 #' 
 require_now <- function(pkgs, update=FALSE, ...) {
-	if(update)
+	if(update) {
+		message("Updating packages...")
 		update.packages(ask=FALSE)
+	}
 	new_pkgs <- pkgs[!pkgs %in% installed.packages()[ ,"Package"]]
-	message(paste0("The packages '", paste(pkgs[!pkgs %in% new_pkgs],
-							collapse="' '"), "' are already installed."))
+	old_pkgs <- pkgs[!pkgs %in% new_pkgs]
+	# Grammatically correct message
+	if(length(old_pkgs) == 1)
+		message(paste0("The package '", old_pkgs, "' is already installed ",
+						"and will not be installed now."))
+	if(length(old_pkgs) > 1)
+		message(paste0("The packages '",
+						paste(old_pkgs[1:(length(old_pkgs) - 1)],
+								collapse="', '"), "' and '",
+						old_pkgs[length(old_pkgs)], "' are already installed."))
 	if(length(new_pkgs) > 0) install.packages(new_pkgs, ...)
+	## message("Loading packages into session.")
+	sapply(pkgs, require, character.only=TRUE)
+	message("DONE!")
 }
